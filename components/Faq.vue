da527cac0ff7718d6655d38458586c54e82010da
Alan Hilarion <alan.hilarion@proton.me>
2025-01-28 00:44:50 +0100

    ✨ feat: added loading state to FAQ component and fetched data from API

--- components/Faq.vue
+++ components/Faq.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ArrowLongRightIcon, PhoneIcon } from "@heroicons/vue/24/solid";
+import {ArrowPathIcon} from "@heroicons/vue/24/outline";
 
 const props = defineProps<{
+  loading: boolean;
   faq: {
     question: string;
     answer: string;
@@ -16,7 +17,11 @@ const props = defineProps<{
       <h2 class="text-2xl font-bold">Vous avez une <span class="text-primary">question</span> ?</h2>
       <p class="text-gray-600 mt-4">Vous trouverez ici les réponses aux questions les plus fréquemment posées par nos clients. Si vous ne trouvez pas la réponse à votre question, n'hésitez pas à nous contacter.</p>
     </div>
-    <div class="flex flex-col gap-4 w-full md:w-3/4 items-center justify-center md:items-start">
+    <div
+      v-if="props.loading" class="flex items-center justify-center w-full md:w-3/4 h-[300px] rounded-lg">
+      <ArrowPathIcon class="w-10 h-10 text-primary animate-spin" />
+    </div>
+    <div v-else class="flex flex-col gap-4 w-full md:w-3/4 items-center justify-center md:items-start">
       <FaqQuestion v-for="(faq, index) in props.faq" :key="index" :question="faq.question" :answer="faq.answer" />
     </div>
   </div>
--- pages/faq.vue
+++ pages/faq.vue
@@ -5,34 +5,33 @@
     <p class="text-primary font-bold">/</p>
     <p class="text-primary font-bold">FAQ</p>
   </TreeBanner>
-  <Faq :faq="faq" />
+  <div>
+    <Faq :faq="faq" :loading="loading" />
+  </div>
   <Footer />
   <img src="~/assets/SLIDE_02.jpg" alt="slide" class="fixed top-0 left-0 w-full h-full object-cover -z-10" />
 </template>
 
 <script setup lang="ts">
-const faq = [
-  {
-    question: "Puis-je utiliser mon chèque énergie pour l'achat de pellets ?",
-    answer: "Oui, vous pouvez utiliser votre chèque énergie pour l'achat de pellets. Le chèque énergie est une aide financière destinée à aider les ménages à faibles revenus à payer leurs factures d'énergie. Il peut être utilisé pour l'achat de pellets, qui sont le combustible utilisé par les poêles à granulés."
-  },
-  {
-    question: "Quels sont les avantages des poêles à granulés ?",
-    answer: "Les poêles à granulés sont une alternative écologique et économique aux poêles à bois traditionnels. Ils sont faciles à utiliser, efficaces et permettent de réaliser des économies d'énergie."
-  },
-  {
-    question: "Comment entretenir un poêle à granulés ?",
-    answer: "Pour entretenir un poêle à granulés, il est recommandé de nettoyer régulièrement le brûleur, le réservoir à granulés et le conduit d'évacuation des fumées. Il est également important de faire appel à un professionnel pour l'entretien annuel du poêle."
-  },
-  {
-    question: "Quelle est la durée de vie d'un poêle à granulés ?",
-    answer: "La durée de vie d'un poêle à granulés dépend de plusieurs facteurs, notamment de la qualité du poêle, de son entretien et de son utilisation. En général, un poêle à granulés peut durer entre 10 et 20 ans."
-  },
-  {
-    question: "Quelle est la différence entre un poêle à granulés et un poêle à bois ?",
-    answer: "La principale différence entre un poêle à granulés et un poêle à bois réside dans le combustible utilisé. Les poêles à granulés fonctionnent avec des granulés de bois, tandis que les poêles à bois fonctionnent avec des bûches de bois. Les poêles à granulés sont également plus faciles à utiliser et plus écologiques que les poêles à bois."
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const faq = ref([]);
+const loading = ref(true);
+
+onMounted(async () => {
+  loading.value = true;
+  try {
+    const response = await axios.get(import.meta.env.VITE_API_URL + '/api/faq');
+    if (response.data.success) {
+      faq.value = response.data.data;
+    }
+  } catch (error) {
+    console.error("Error fetching FAQ:", error);
+  } finally {
+    loading.value = false;
   }
-]
+});
 
 useHead({
   title: "FAQ - Granulés de Bois Picard",
