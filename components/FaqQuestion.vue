603bf19f06ad76a81fd315a40fc98a78731ccf31
Alan Hilarion <alan.hilarion@proton.me>
2025-02-14 06:58:28 +0100

    ✨ fix: correct text alignment in FaqQuestion.vue header for improved readability

--- components/FaqQuestion.vue
+++ components/FaqQuestion.vue
@@ -37,7 +37,7 @@ onBeforeUnmount(() => {
 <template>
   <div class="bg-gray-100 p-6 rounded-lg transition-all duration-500 w-full select-none">
     <button class="flex items-center justify-between w-full focus:outline-none" @click="toggleAccordion">
-      <h1 class="font-semibold text-lefttext-gray-700">{{ props.question }}</h1>
+      <h1 class="font-semibold text-left text-gray-700">{{ props.question }}</h1>
       <span class="text-gray-400 bg-gray-200 rounded-full transition-transform duration-300" :class="{ 'rotate-180': isOpen }">
         <svg v-if="isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
