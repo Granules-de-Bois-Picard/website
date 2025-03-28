961adacfd73886cafb85cc41e64588dd27457849
ahilarion <alan.hilarion@proton.me>
2025-03-28 10:51:01 +0100

    style(Header): adjust header height for medium screens
    Update the header height from `md:h-24` to `md:h-28` to improve visual consistency across different screen sizes.

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@ import Navigation from "@/components/Navigation.vue";
 </script>
 
 <template>
-  <header class="absolute z-30 bg-opacity-50 bg-black h-16 md:h-24 w-full flex items-center justify-between marge">
+  <header class="absolute z-30 bg-opacity-50 bg-black h-16 md:h-28 w-full flex items-center justify-between marge">
     <NuxtLink to="/" class="text-white text-2xl">
       <img src="@/assets/logo.png" alt="logo" class="h-24 hidden lg:block" />
       <img src="@/assets/favicon.png" alt="logo" class="h-16 lg:hidden" />
