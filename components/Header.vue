bb294b05c14affec4e9b3dea04832675a8f50335
ahilarion <alan.hilarion@proton.me>
2025-03-28 10:48:13 +0100

    style(Header): adjust logo and favicon height for better responsiveness
    Updated the height of the logo and favicon in the Header component to improve visual consistency across different screen sizes. The changes ensure a more balanced appearance on both mobile and desktop views.

--- components/Header.vue
+++ components/Header.vue
@@ -6,8 +6,8 @@ import Navigation from "@/components/Navigation.vue";
 <template>
   <header class="absolute z-30 bg-opacity-50 bg-black h-16 md:h-24 w-full flex items-center justify-between marge">
     <NuxtLink to="/" class="text-white text-2xl">
-      <img src="@/assets/logo.png" alt="logo" class="h-16 hidden lg:block" />
-      <img src="@/assets/favicon.png" alt="logo" class="h-12 lg:hidden" />
+      <img src="@/assets/logo.png" alt="logo" class="h-24 hidden lg:block" />
+      <img src="@/assets/favicon.png" alt="logo" class="h-16 lg:hidden" />
     </NuxtLink>
     <Navigation />
   </header>
