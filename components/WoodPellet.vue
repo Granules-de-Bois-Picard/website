c4abb0839408152091f269ef8a4a3f4a107fe1b9
ahilarion <alan.hilarion@proton.me>
2025-04-04 01:16:50 +0200

    refactor(WoodPellet): update NuxtLink destination to /news/les-granules-de-bois
    Changed the destination of the NuxtLink component to point to "/news/les-granules-de-bois" instead of the root "/". This update aligns the link with the intended content path for better user navigation.

--- components/WoodPellet.vue
+++ components/WoodPellet.vue
@@ -41,7 +41,7 @@ import { ArrowLongRightIcon, PhoneIcon } from "@heroicons/vue/24/solid";
         </li>
       </ul>
       <NuxtLink
-          to="/"
+          to="/news/les-granules-de-bois"
           class="text-white mt-10 bg-primary px-8 py-4 rounded-full flex items-center gap-2 w-fit hover:bg-white hover:text-black transition-all duration-300 ease-in-out border-2 border-primary hover:border-gray-800"
       >
         Découvrir
@@ -50,7 +50,7 @@ import { ArrowLongRightIcon, PhoneIcon } from "@heroicons/vue/24/solid";
     </div>
     <div class="relative flex-1 flex items-center justify-center">
       <div class="relative w-2/3">
-        <img src="https://gp-api.luwa.fr/storage/files/67a2b41f64690_1738716191.png" alt="slide" class="object-cover w-full" />
+        <img src="https://api.granulespicard.fr/storage/files/67a2b41f64690_1738716191.png" alt="slide" class="object-cover w-full" />
         <div class="absolute top-0 right-0 w-1/3 transform translate-x-1/3 min-w-[100px] min-h-[100px]">
           <img src="@/assets/stamp.png" alt="fire" class="h-auto object-cover animate-spin-slow" />
         </div>
